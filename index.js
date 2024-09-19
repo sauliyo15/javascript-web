4af4afb92a119a16c5141b6d13da47ff490772e2
sauliyo15 <gc.saul.7@gmail.com>
2024-09-19 20:36:43 +0200

    Using environment variables

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
+// Cargar las variables de entorno
+require('dotenv').config();
+
+// Usar el puerto desde variables de entorno o el 3000 por defecto
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('¡Hola Mundo!');
